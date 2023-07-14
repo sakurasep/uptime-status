d7ed96bbb21ad7c5eddaf3e5815755e6a68897f2
上杉九月 <13562008871@163.com>
2023-07-14 15:45:16 +0800

    更新配置

--- public/config.js
+++ public/config.js
@@ -6,10 +6,7 @@ window.Config = {
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only
   ApiKeys: [
-    'm784488775-dd1ad84b209c05f8e185c33e',
-    'm784490063-7b5da437e7f1e0d67613714d',
-    'm784497419-de55aa09902ccb3ab22d548a',
-    'm784496436-71a4bf7b1e3bdf7756be131b',
+    'ur1286723-4ea6582b418487da3826297a',
   ],
 
   // 日志天数
@@ -21,16 +18,16 @@ window.Config = {
   // 导航栏菜单
   Navi: [
     {
-      text: 'Homepage',
-      url: 'https://status.org.cn/'
+      text: '个人主页',
+      url: 'https://sakurasep.site'
     },
     {
       text: 'GitHub',
-      url: 'https://github.com/yb/uptime-status'
+      url: 'https://github.com/sakurasep'
     },
     {
       text: 'Blog',
-      url: 'https://abo.xyz/'
+      url: 'https://blog.sakurasep.site'
     },
   ],
 };
